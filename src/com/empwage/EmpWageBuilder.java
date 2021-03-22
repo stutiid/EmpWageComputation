6fd8afcd3198b26695ee3b251713fcacd966d565
stutiid <stutichaturvedi12@gmail.com>
2021-03-22 18:18:30 +0530

    Added: employee monthly salary

--- src/com/empwage/EmpWageBuilder.java
+++ src/com/empwage/EmpWageBuilder.java
@@ -8,21 +8,28 @@ public static void main(String args[]) {
 	final int IS_FULL_TIME=1;
 	final int EMP_RATE_PER_HR=20;
 	final int IS_PART_TIME=2;
+	final int TOTAL_WORKING_DAYS=20;
 	//variables
 	double empCheck;
 	int empHrs=0;
-	int empWage=0;
+	int totalEmpWage=0;
+	int totalWorkingHrs=0;
 	//computation
-	empCheck=Math.floor(Math.random()*10)%3;
-	switch ((int)empCheck)
+	for(int days=0;days<TOTAL_WORKING_DAYS;days++)
 	{
+		empCheck=Math.floor(Math.random()*10)%3;
+		switch ((int)empCheck)
+		{
 			case IS_FULL_TIME: empHrs=8;
-							   break;
+									 break;
 			case IS_PART_TIME: empHrs=4;
-							   break;
+									  break;
 			default : empHrs=0;
+		}
+		System.out.println(empHrs);
+		totalWorkingHrs+=empHrs;
 	}
-	empWage=empHrs*EMP_RATE_PER_HR;
-	System.out.println("EmpWage = "+empWage);
+	totalEmpWage=totalWorkingHrs*EMP_RATE_PER_HR;
+	System.out.println("EmpWage = "+totalEmpWage);
 }
 }
