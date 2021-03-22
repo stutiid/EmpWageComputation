921b187c51a6aa59490cc917842f646f69665686
stutiid <stutichaturvedi12@gmail.com>
2021-03-22 18:09:28 +0530

    Added: employee daily wage using switch

--- src/com/empwage/EmpWageBuilder.java
+++ src/com/empwage/EmpWageBuilder.java
@@ -14,12 +14,14 @@ public static void main(String args[]) {
 	int empWage=0;
 	//computation
 	empCheck=Math.floor(Math.random()*10)%3;
-	if(empCheck==IS_FULL_TIME)
-		empHrs=8;
-	else if(empCheck==IS_PART_TIME)
-		empHrs=4;
-	else
-		empHrs=0;
+	switch ((int)empCheck)
+	{
+			case IS_FULL_TIME: empHrs=8;
+							   break;
+			case IS_PART_TIME: empHrs=4;
+							   break;
+			default : empHrs=0;
+	}
 	empWage=empHrs*EMP_RATE_PER_HR;
 	System.out.println("EmpWage = "+empWage);
 }
