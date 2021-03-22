277108460898586b079aa2e837cf447df5a8945a
stutiid <stutichaturvedi12@gmail.com>
2021-03-22 17:47:59 +0530

    Added: Emplooyee Attendance

--- src/com/empwage/EmpWageBuilder.java
+++ src/com/empwage/EmpWageBuilder.java
@@ -2,6 +2,17 @@ package com.empwage;
 
 public class EmpWageBuilder {
 public static void main(String args[]) {
+	//welcome message
 	System.out.println("Welcome to employee wage computation problem");
+	//constants
+	final int IS_FULL_TIME=1;
+	//variables
+	double empCheck;
+	//computation
+	empCheck=Math.floor(Math.random()*10)%2;
+	if (empCheck==IS_FULL_TIME)
+		System.out.println("Employee is Present");
+	else
+		 System.out.println("Employee is Absent");
 }
 }
