d09e6dc8d7263dd407dd0f703fcdc7a7e870c5ef
stutiid <stutichaturvedi12@gmail.com>
2021-03-23 09:24:12 +0530

    Added: calculate EmpWage until maxium hrs is reached or maximum days is reached

--- src/com/empwage/EmpWageBuilder.java
+++ src/com/empwage/EmpWageBuilder.java
@@ -1,26 +1,25 @@
 package com.empwage;
 
 public class EmpWageBuilder {
+	//constants
+	final static int IS_FULL_TIME=1;
+	final static int EMP_RATE_PER_HR=20;
+	final static int IS_PART_TIME=2;
+	final static int TOTAL_WORKING_DAYS=20;
+	final static int TOTAL_WORKING_HR=100;
 public static void main(String args[]) {
 	//welcome message
 	System.out.println("Welcome to employee wage computation problem");
-	//constants
-	final int IS_FULL_TIME=1;
-	final int EMP_RATE_PER_HR=20;
-	final int IS_PART_TIME=2;
-	final int TOTAL_WORKING_DAYS=20;
-	final int TOTAL_WORKING_HR=100;
 	//variables
-	double empCheck;
-	int empHrs=0;
 	int totalEmpWage=0;
 	int totalWorkingHrs=0;
 	int days=0;
 	//computation
-	while(days<TOTAL_WORKING_DAYS && totalWorkingHrs<TOTAL_WORKING_HR)
+	while(days<TOTAL_WORKING_DAYS && totalWorkingHrs<=TOTAL_WORKING_HR)
 	{
 		days++;
-		empCheck=Math.floor(Math.random()*10)%3;
+		int empHrs=0;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		switch ((int)empCheck)
 		{
 			case IS_FULL_TIME: empHrs=8;
