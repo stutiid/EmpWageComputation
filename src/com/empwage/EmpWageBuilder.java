817a37479b5612e9e7935671d0cae61d9d3585c2
stutiid <stutichaturvedi12@gmail.com>
2021-03-24 11:26:13 +0530

    Added: introduced part time

--- src/com/empwage/EmpWageBuilder.java
+++ src/com/empwage/EmpWageBuilder.java
@@ -1,26 +1,27 @@
 package com.empwage;
 
 public class EmpWageBuilder {
-public static void main(String args[]) {
-	//welcome message
-	System.out.println("Welcome to employee wage computation problem");
-	//constants
-	final int IS_FULL_TIME=1;
-	final int EMP_RATE_PER_HR=20;
-	final int IS_PART_TIME=2;
-	//variables
-	double empCheck;
-	int empHrs=0;
-	int empWage=0;
-	//computation
-	empCheck=Math.floor(Math.random()*10)%3;
-	if(empCheck==IS_FULL_TIME)
-		empHrs=8;
-	else if(empCheck==IS_PART_TIME)
-		empHrs=4;
-	else
-		empHrs=0;
-	empWage=empHrs*EMP_RATE_PER_HR;
-	System.out.println("EmpWage = "+empWage);
-}
+	// constants
+	public final static int IS_FULL_TIME = 1;
+	public final static int EMP_RATE_PER_HR = 20;
+	public final static int IS_PART_TIME = 2;
+
+	public static void main(String args[]) {
+		// welcome message
+		System.out.println("Welcome to employee wage computation problem");
+		// variables
+		double empCheck;
+		int empHrs = 0;
+		int empWage = 0;
+		// checking how many hrs employee has worked
+		empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+		else
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HR;
+		System.out.println("EmpWage for a day = " + empWage);
+	}
 }
