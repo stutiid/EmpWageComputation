46d548dad6925d61146b8fcf916a1bd63ab4e60b
stutiid <stutichaturvedi12@gmail.com>
2021-03-24 11:21:41 +0530

    Added:calculating empWage for a day

--- src/com/empwage/EmpWageBuilder.java
+++ src/com/empwage/EmpWageBuilder.java
@@ -1,23 +1,24 @@
 package com.empwage;
 
 public class EmpWageBuilder {
-public static void main(String args[]) {
-	//welcome message
-	System.out.println("Welcome to employee wage computation problem");
-	//constants
-	final int IS_FULL_TIME=1;
-	final int EMP_RATE_PER_HR=20;
-	//variables
-	double empCheck;
-	int empHrs=0;
-	int empWage=0;
-	//computation
-	empCheck=Math.floor(Math.random()*10)%3;
-	if(empCheck==IS_FULL_TIME)
-		empHrs=8;
-	else
-		empHrs=0;
-	empWage=empHrs*EMP_RATE_PER_HR;
-	System.out.println("EmpWage = "+empWage);
-}
+	public static void main(String args[]) {
+		// welcome message
+		System.out.println("Welcome to employee wage computation problem");
+		// constants
+		final int IS_FULL_TIME = 1;
+		final int EMP_RATE_PER_HR = 20;
+		// variables
+		double empCheck;
+		int empHrs = 0;
+		int empWage = 0; // initial wage
+		// computation
+		empCheck = Math.floor(Math.random() * 10) % 3;
+		// checking how many hrs employee has worked
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HR;
+		System.out.println("EmpWage = " + empWage);
+	}
 }
